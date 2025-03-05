a759a387be8d8d377634e0b6531e8553ee8fa7a3
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-03-05 21:20:22 +0100

    📝 Commit: Update appwrite.

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Account, Databases } from 'appwrite';
+import { Client, Account, Databases, Query } from 'appwrite';
 
 export const client = new Client();
 
@@ -8,4 +8,4 @@ client
 
 export const account = new Account(client);
 export const databases = new Databases(client);
-export { ID } from 'appwrite';
+export { ID, Query } from 'appwrite';
