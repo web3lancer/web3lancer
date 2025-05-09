9b0ea747da1ddc496beca6a68c6f7f3f23e19e7a
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-05-09 07:38:42 +0000

    generate idx configs for project

---
+++ .idx/dev.nix
@@ -0,0 +1,31 @@
+{pkgs}: {
+
+  services.docker.enable = true;
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.pnpm
+    pkgs.gh
+    
+  ];
+  idx.extensions = [
+    
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "pnpm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
